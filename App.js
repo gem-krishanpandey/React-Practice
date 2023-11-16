7fdeb55a23fe21e021d8f0998acbe411e00ab945
Krishan Chandra Pandey <Krishan.Pandey@Geminisolutions.com>
2023-11-16 23:09:46 +0530

    First react code

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("div",{id: "parent"},React.createElement("div",{id: "child"},[React.createElement("h1",{id: "heading"},"This is heading 1"),React.createElement("h2",{id: "heading"},"This is heading 2")]));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading)
